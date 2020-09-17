cd4f948817b0080612e2dfb1d069de6999324adc
Sergey <Prostov>
2020-09-17 11:08:27 +0300

    vert layout builded

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ let x = setInterval(function() {
   let now = new Date().getTime()
   let distance = countDownDate - now
 
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  // let days = Math.floor(distance / (1000 * 60 * 60 * 24))
   let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
   let seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-  document.getElementById('demo').innerHTML = days + 'д ' + hours + 'ч ' + minutes + 'м ' + seconds + 'с '
+  // document.getElementById('demo').innerHTML = days + 'д ' + hours + 'ч ' + minutes + 'м ' + seconds + 'с '
+  document.getElementById('demo').innerHTML = hours + ':' + minutes + ':' + seconds
 
   if (distance < 0) {
     clearInterval(x)
